5bce7527c343f90b19e11f2d2eba6d92ed851ce4
chinmairam <chinmairammuppa@gmail.com>
2021-04-06 22:15:33 +0530

    Deployment

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final = [np.array(int_features)].reshape(1, 4)
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features)]
     predictions = model1.predict(final)
     output = predictions[0]
     return render_template('predict.html', prediction_text="Prediction is {}".format(output))
 
-    if int(predictions) == 0:
+    if predictions == 0:
         return render_template('predict.html', prediction_text=f'Sorry,Your loan was not approved')
     else:
         return render_template('predict.html', prediction_text=f'Your loan was approved')
